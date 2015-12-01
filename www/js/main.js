7d89252c79c3a84f158bb1208a25aea87a3bfe34
Trevor Allen <tallen94@uw.edu>
2015-12-01 15:39:41 -0800

    adds frame for index

---
+++ www/js/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+angular.module("LunchDate", ["ui.router", "ngSanitize"])
+.config(function($stateProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'partials/home.html',
+			controller: 'HomeCtrl'
+		})
+		.state('login', {
+			url: '/login',
+			templateUrl: 'partials/login.html',
+			controller: 'LoginCtrl'
+		})
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'partials/signup.html',
+			controller: 'SignupCtrl'
+		})
+		.state('create-date', {
+			url: '/create-date',
+			templateUrl: 'createlunchdate.html',
+			controller: 'CreateLunchDateCtrl'
+		})
+})
+
+.controller("MainCtrl", ['$scope', '$state', function($scope, $state) {
+
+	$scope.uiRouterState = $state;
+
+	$scope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams) {
+		$scope.uiRouterState = $state;
+	});
+}])

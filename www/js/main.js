fa19aefabc0dc63c2e357dd4c49674e9cbe9cd24
jakersnorth <johnfakersnorth@gmail.com>
2015-12-08 16:38:56 -0800

    add functioning password confirm

--- www/js/main.js
+++ www/js/main.js
@@ -72,3 +72,26 @@ angular.module('LunchDate', ['ui.router', 'ngSanitize']).run(function() {
         $http.get('https://api.yelp.com/v2/search?term=food&location=')
     }
 }])
+
+.directive('sameAs', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elem, attrs, ngModel) {
+            ngModel.$parsers.unshift(validate);
+
+            // Force-trigger the parsing pipeline.
+            scope.$watch(attrs.sameAs, function() {
+                ngModel.$setViewValue(ngModel.$viewValue);
+            });
+
+            // Checks for validity
+            function validate(value) {
+                var isValid = scope.$eval(attrs.sameAs) == value;
+
+                ngModel.$setValidity('same-as', isValid);
+
+                return isValid ? value : undefined;
+            }
+        }
+    };
+});
